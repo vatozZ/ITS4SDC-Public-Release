d1c9a37592f246e8712f19cfd203587e57f4d381
vatozZ <46943127+vatozZ@users.noreply.github.com>
2025-01-04 16:51:05 +0200

    Road feature extractor
    This script extracts the road features (segment angles, segment lengths) and saves them into a JSON file.

---
+++ extract_road_characteristics.py
@@ -0,0 +1,59 @@
+"""
+It reads a JSON file that contains road coordinates and corresponding labels (pass, fail).
+It extracts the (segment_angles, segment_lengths) features for each road.
+A segment angle is the difference between the angles of two consecutive segments.
+A segment length is the Euclidean distance between two road coordinates.
+It saves the features in a JSON file.
+
+18 November 2024.
+@vatozZ
+"""
+
+filename = 'dataset_10000.json'
+
+import pandas as pd
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open(filename, 'r') as f:
+    jsondata = json.load(f)
+
+segment_angles_all_list = []
+segment_lengths_all_list = []
+label_list = []
+
+for test_case in jsondata:
+
+    roadPointsArray = np.array(test_case['road_points'])
+
+    # 1) Calculate segment angles
+    dx = roadPointsArray[1:, 0] - roadPointsArray[:-1, 0]
+    dy = roadPointsArray[1:, 1] - roadPointsArray[:-1, 1]
+
+    raw_angles = np.rad2deg(np.arctan2(dy, dx))
+    segment_angles = np.zeros_like(raw_angles)
+    segment_angles[1:] = np.diff(raw_angles)
+
+    segment_angles_all_list.append(list(segment_angles))
+
+    # 2) Calculate segment lengths
+
+    _differences = roadPointsArray[1:] - roadPointsArray[:-1]
+    segment_lengths = list(np.linalg.norm(_differences, axis=1))
+
+    segment_lengths_all_list.append(segment_lengths)
+
+    # 3) Assign labels
+    
+    if test_case['test_outcome'] == 'FAIL': label_list.append(0)
+    elif test_case['test_outcome'] == 'PASS': label_list.append(1)
+    else: quit("ERROR!")
+
+with open(filename.split('.json')[0] + '_road_characteristics.json', 'w') as f:
+    json.dump(
+        {'segment_angles': segment_angles_all_list,
+         'segment_lengths': segment_lengths_all_list,
+         'labels': label_list}, f, indent=4)
+
+print("Pre Processing and Saving Has Completed!")
