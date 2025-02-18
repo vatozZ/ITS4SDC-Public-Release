e8873382009a4fc750531757d54c07cd0d47c578
vatozZ <46943127+vatozZ@users.noreply.github.com>
2025-02-18 10:45:52 +0200

    Update combine_datasets.py
    Argument parser added.

--- combine_datasets.py
+++ combine_datasets.py
@@ -4,20 +4,27 @@ It gets the dataset that has test files, and it merges into a single file.
 
 Merged file stores 'road_points' and 'test_outcome'.
 
+All test cases should be in .json file format.
+
 16 November 2024.
 """
 
-files_path = r'C:\Users\monster\Desktop\dataset_10000'
-
-invalid_file_path = r'C:\Users\monster\Desktop\dataset_10000'
-
-import shutil
+import argparse
+from scipy.interpolate import interp1d
 import json, os
 import numpy as np
-from shapely.geometry import LineString
 from numpy import arange
 import pandas as pd
-from scipy.interpolate import interp1d
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description='Test cases combiner. Provide dataset path.')
+parser.add_argument('--path', type=str, help='Provide dataset folder name.')
+
+args = parser.parse_args()
+
+dataset_path = args.path
+
+data_list = []
 
 def adjust_array_size(array, target_size=197):
     
@@ -40,31 +47,27 @@ def adjust_array_size(array, target_size=197):
         interpolated_y = interpolator_y(target_indices)
 
         return np.column_stack((interpolated_x, interpolated_y))
-        
 
-invalid = 0
-
-data_list = []
 # iterate all files in the directory
-for file in os.listdir(files_path):
+for file in tqdm(os.listdir(dataset_path)):
     if file.endswith('.json'):
         data_dict = {}
         # open json file
-        with open(files_path + '/' + file, 'r') as f:
+        with open(dataset_path + '/' + file, 'r') as f:
             jsonfile = json.load(f)
-            if jsonfile['is_valid'] == False:
-                invalid += 1
-                shutil.copy(files_path + '/' + file, invalid_file_path + '/')
-                continue
-
-            data_dict['road_points'] = adjust_array_size(np.array(jsonfile['interpolated_points']))
+            
+            if 'interpolated_points' in jsonfile.keys():
+                data_dict['road_points'] = adjust_array_size(np.array(jsonfile['interpolated_points']))
+            
+            elif 'interpolated_road_points' in jsonfile.keys():
+                data_dict['road_points'] = adjust_array_size(np.array(jsonfile['interpolated_road_points']))
+            
             data_dict['test_outcome'] = jsonfile['test_outcome']
+        
         data_list.append(data_dict)
-
-print("Number of invalid tests:", invalid)
-print("Number of valid tests:", len(data_list))
 
 data_frame = pd.DataFrame(data_list)
 
 # write out the dataframe as a JSON file.
-data_frame.to_json('dataset_' + str(data_frame.shape[0]) + '.json', orient='records', indent=2)
+data_frame.to_json('dataset_combined_' + str(data_frame.shape[0]) + '.json', orient='records', indent=2)
+
