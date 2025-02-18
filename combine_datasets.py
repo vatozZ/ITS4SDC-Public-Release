a046024fbf9fb1f1716581e238cd1f7a3aaef985
vatozZ <46943127+vatozZ@users.noreply.github.com>
2025-02-18 10:33:49 +0200

    Update combine_datasets.py

--- combine_datasets.py
+++ combine_datasets.py
@@ -6,7 +6,6 @@ Merged file stores 'road_points' and 'test_outcome'.
 
 16 November 2024.
 """
-from utils import adjust_array_size
 
 files_path = r'C:\Users\monster\Desktop\dataset_10000'
 
@@ -18,6 +17,29 @@ import numpy as np
 from shapely.geometry import LineString
 from numpy import arange
 import pandas as pd
+
+def adjust_array_size(array, target_size=197):
+    
+    if array.shape[0] == target_size:
+        return array
+    
+    elif array.shape[0] > target_size:
+        indices = np.linspace(0, array.shape[0] - 1, target_size, dtype=int)
+        return array[indices]
+    
+    
+    else:
+        current_indices = np.linspace(0, array.shape[0] - 1, array.shape[0])
+        target_indices = np.linspace(0, array.shape[0] - 1, target_size)
+
+        interpolator_x = interp1d(current_indices, array[:, 0], kind='linear')
+        interpolator_y = interp1d(current_indices, array[:, 1], kind='linear')
+
+        interpolated_x = interpolator_x(target_indices)
+        interpolated_y = interpolator_y(target_indices)
+
+        return np.column_stack((interpolated_x, interpolated_y))
+        
 
 invalid = 0
 
